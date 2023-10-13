f1987bdf739099a00bc03268bfacb2a267ad9ba7
Nicolas <nicolas.corgom@gmail.com>
2023-10-13 10:36:24 -0500

    Prueba

--- src/screens/Pokedex.js
+++ src/screens/Pokedex.js
@@ -5,7 +5,7 @@ import React from 'react'
 export default function Pokedex() {
     return (
         <SafeAreaView>
-            <Text>Agua</Text>
+            <Text>PokeDex</Text>
         </SafeAreaView>
     )
 }
