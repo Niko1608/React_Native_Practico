f52615eb521a865a344ccc38f7e45d49d9f76e8b
Nicolas <nicolas.corgom@gmail.com>
2023-10-13 11:33:20 -0500

    Clase_5

--- src/screens/Pokedex.js
+++ src/screens/Pokedex.js
@@ -1,8 +1,24 @@
+import React, { useState, useEffect } from 'react'
 import { Text } from 'react-native'
 import { SafeAreaView } from "react-native-safe-area-context";
-import React from 'react'
+import { getPokemonApi } from '../api/pokemon';
 
 export default function Pokedex() {
+    useEffect(() => {
+        (async () => {
+            await loadPokemons();
+        })();
+    }, []);
+
+    const loadPokemons = async () => {
+        try {
+            const response = await getPokemonApi();
+            console.log(response);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
     return (
         <SafeAreaView>
             <Text>PokeDex</Text>
