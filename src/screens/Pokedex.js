a4d775635e0bcc8e203c7f4e4b000b048d4aee9a
Nicolas <nicolas.corgom@gmail.com>
2023-10-17 12:07:57 -0500

    Clase 6

--- src/screens/Pokedex.js
+++ src/screens/Pokedex.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import { Text } from 'react-native'
 import { SafeAreaView } from "react-native-safe-area-context";
-import { getPokemonApi } from '../api/pokemon';
+import { getPokemonApi, getPokemonDetailsByUrlApi } from '../api/pokemon';
 
 export default function Pokedex() {
+    const [pokemons, setPokemons] = useState([]);
+    console.log("pokemons--->", pokemons);
+
     useEffect(() => {
         (async () => {
             await loadPokemons();
@@ -13,7 +16,22 @@ export default function Pokedex() {
     const loadPokemons = async () => {
         try {
             const response = await getPokemonApi();
-            console.log(response);
+
+            const pokemonArray = [];
+            for await (const pokemon of response.results) {
+                const pokemonDetails = await getPokemonDetailsByUrlApi(pokemon.url);
+                console.log(pokemonDetails);
+
+                pokemonArray.push({
+                    id: pokemonDetails.Id,
+                    name: pokemonDetails.name,
+                    types: pokemonDetails.types[0].type.name,
+                    order: pokemonDetails.order,
+                    imagen: pokemonDetails.sprites.other['oficial-artwork'].front_default
+                });
+            }
+
+            setPokemons([...pokemons, ...pokemonArray]);
         } catch (error) {
             console.error(error);
         }
