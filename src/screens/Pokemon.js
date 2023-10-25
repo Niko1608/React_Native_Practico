6742b6870640aaacf0dfff3eb39d1a90f7ca51e9
Nicolas Cortes <nicolas.corgom@gmail.com>
2023-10-25 08:42:54 -0500

    Clase 16

--- src/screens/Pokemon.js
+++ src/screens/Pokemon.js
@@ -2,6 +2,8 @@ import { ScrollView } from 'react-native'
 import React, { useState, useEffect } from 'react'
 import { getPokemonDetailsApi } from "../api/pokemon"
 import Header from "../components/Header";
+import Type from "../components/Type";
+import Stats from '../components/Stats';
 
 export default function Pokemon(props) {
     const {
@@ -31,6 +33,8 @@ export default function Pokemon(props) {
                 image={pokemon.sprites.other["official-artwork"].front_default}
                 type={pokemon.types[0].type.name}
             />
+            <Type types={pokemon.types} />
+            <Stats stats={pokemon.stats} />
         </ScrollView>
     );
 }
